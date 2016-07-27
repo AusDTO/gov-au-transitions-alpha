ad444c9dfc65fb5f8c1cf894de1bbcc5f55a0c0a
Alex Riolo <alexriolo@DTOAdmins-MacBook-Pro-8.local>
2016-07-27 12:00:46 +1000

    added skeleton for webpack to work

---
+++ _assets/javascripts/app.js
@@ -0,0 +1,6 @@
+import 'babel-polyfill';
+
+let questionsHolder = document.getElementById("questions");
+let newText = "this is a test to see if the js will run";
+
+questionsHolder.innerHTML = newText;
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: './_assets/javascripts/app.js',
+    output: {
+        path: './_assets/javascripts/bin',
+        filename: 'app.bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+        }]
+    },
+    watch: true
+};
