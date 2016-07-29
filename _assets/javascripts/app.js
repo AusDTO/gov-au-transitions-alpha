1bbe4bbf931d37f61bab9c787d5c9d9173430551
Alex Riolo <alexriolo@DTOAdmins-MacBook-Pro-8.local>
2016-07-29 10:32:32 +1000

    added redux framework

--- _assets/javascripts/app.js
+++ _assets/javascripts/app.js
@@ -1,6 +1,15 @@
-import 'babel-polyfill';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import transitionApp from './reducers'
+import Question from './components/Question'
 
-let questionsHolder = document.getElementById("questions");
-let newText = "this is a test to see if the js will run";
+let store = createStore(transitionApp)
 
-questionsHolder.innerHTML = newText;
+render(
+  <Provider store={store}>
+    <Question />
+  </Provider>,
+  document.getElementById('questions')
+)
