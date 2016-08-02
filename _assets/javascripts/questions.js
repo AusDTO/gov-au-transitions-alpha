d6e39c057c682b9804a57539b6097f8567d9abcb
Alex Riolo <alexriolo@DTOAdmins-MacBook-Pro-8.local>
2016-08-02 14:07:32 +1000

    added new questions to this sprint

--- _assets/javascripts/questions.js
+++ _assets/javascripts/questions.js
@@ -6,52 +6,80 @@ export const QuestionFlow = {
       type: "checkbox",
       legend: "Select as many as apply",
       values: [
-        {value: "ownhome", label: "Own home"},
+        {value: "athome", label: "At home"},
         {value: "family", label: "Family or friend’s home"},
-        {value: "retirementvillage", label: "Retirement village"},
         {value: "nursinghome", label: "Nursing home"},
+        {value: "retirementvillage", label: "Retirement village"},
         {value: "nolive", label: "I don't know"}
       ]
     },
     {
-      question: "What do you think they need help with?",
-      name: "help",
-      condition: "ownhome,family",
+      question: "What do they need help with?",
+      name: "helpnursinghome",
+      condition: "nursinghome",
       type: "checkbox",
       legend: "Select as many as apply",
       values: [
-        {value: "home", label: "Managing at home"},
-        {value: "meds", label: "Managing medication"},
-        {value: "outandabout", label: "Getting out and about"}
+        {value: "nursinginvolved", label: "What’s involved in getting my older person into a nursing home?"},
+        {value: "nursingprovide", label: "What does a nursing home provide?"},
+        {value: "nursingcosts", label: "What are the costs?"},
+        {value: "nursingfind", label: "How do I find one?"}
       ]
     },
     {
-      question: "Where do they live?",
-      name: "location",
-      condition: "ownhome,family",
-      type: "location",
-      legend: "This helps us to suggest local services.",
-      values: []
-    },
-    {
-      question: "Which area/s are they considering moving to?",
-      legend: "This helps us to suggest local services.",
-      name: "newlocation",
-      condition: "retirementvillage,nursinghome,nolive",
-      type: "locationaddmore",
-      legend: "postcode, town or village",
-      values: []
-    },
-    {
-      question: "Do any of these apply?",
-      name: "entitlement",
+      question: "What do you need help with?",
+      name: "homehelp",
+      condition: "athome,family",
       type: "checkbox",
-      legend: "This helps us to suggest things like subsidised services and the right forms.",
+      legend: "Select as many as apply",
       values: [
-        {value: "pensioner", label: "Age pensioner"},
-        {value: "disability", label: "Disability pensioner"},
-        {value: "veteran", label: "Veteran/war widow or widower"},
-        {value: "aboriginal", label: "Aboriginal or Torres Strait Islander peoples"},
+        {value: "homehelpget", label: "How do they get home help?"},
+        {value: "homehelpkind", label: "What kinds of help are available?"},
+        {value: "homehelpcosts", label: "What are the costs?"},
+        {value: "homehelpfind", label: "How do I find these services?"}
+      ]
+    },
+    {
+      question: "What do you need help with?",
+      legend: "Select as many as apply",
+      name: "newlocation",
+      condition: "retirementvillage",
+      type: "checkbox",
+      values: [
+        {value: "retirementwhat", label: "What’s involved in getting into a retirement village?"},
+        {value: "retirementprovide", label: "What does a retirement village provide?"},
+        {value: "retirementcosts", label: "What are the costs?"},
+        {value: "retirementfind", label: "How do I find retirement villages?"}
+      ]
+    },
+    {
+      question: "What do you need help with?",
+      name: "homeassist",
+      condition: "athome,family",
+      type: "checkbox",
+      legend: "Select as many as apply",
+      values: [
+        {value: "homeassistcooking", label: "Cooking and preparing meals"},
+        {value: "homeassisteating", label: "Eating"},
+        {value: "homeassistcleaning", label: "Cleaning"},
+        {value: "homeassistmodifications", label: "Home modifications"},
+        {value: "homeassistshowering", label: "Showering"},
+        {value: "homeassistdressing", label: "Dressing"},
+        {value: "homeassisttoilet", label: "Going to the toilet"},
+        {value: "homeassistwashing", label: "Hanging out and bringing in the washing"},
+        {value: "homeassistgardening", label: "Gardening"}
+      ]
+    },
+    {
+      question: "What’s involved in getting home modificaitons?",
+      name: "homemods",
+      condition: "athome,family",
+      type: "checkbox",
+      legend: "Select as many as apply",
+      values: [
+        {value: "homemodswhat", label: "What do Home Modifications help me to do?"},
+        {value: "homemodscost", label: "What are the costs?"},
+        {value: "homemodshow", label: "How do I get them?"}
       ]
     }
   ],
