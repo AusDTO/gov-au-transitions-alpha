855fdb1fdafbc113bb4207fa430764ed4c5f7437
Alex Riolo <alexriolo@DTOAdmins-MacBook-Pro-8.local>
2016-08-15 17:29:58 +1000

    changes to the steps results to draw out the list

--- _assets/javascripts/components/ResultSet.js
+++ _assets/javascripts/components/ResultSet.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import Accordion from './Accordion'
 
+const getStepActions = actions => {
+  return actions.map(action => {
+    return (<a key={action.label.split(" ").join("_")}>{action.label}</a>)
+  })
+}
+
 const getAccordionContent = items => {
   switch (items.type) {
     case "result":
-      return (<ul className="result-list">{items.items.map(item => {
+      return (<ul>{items.items.map(item => {
         return (
           <li key={item.label.split(" ").join("_")}>
             <span className="number">{item.number}</span>
-            <a href={item.link}>{item.label}</a>
+            <a rel="external" href={item.link}>{item.label}</a>
           </li>
         )
       })}</ul>)
     case "steps":
-      return (<form><ul className="steps-list">{items.items.map(item => {
+      return (<form><ul>{items.items.map(item => {
         let name = item.label.split(" ").join("_")
         return (
           <li key={name}>
@@ -24,21 +30,27 @@ const getAccordionContent = items => {
               checked={false} />
             <label htmlFor={name}>{item.label}</label>
             <p>{item.abstract}</p>
+            <p>{getStepActions(item.actions)}</p>
           </li>)
       })}</ul></form>)
       return (<div>Step type set</div>)
       break;
     case "information":
     default:
-      return (<ul className="info-list">{items.items.map(item => {
-        return (<li key={item.label.split(" ").join("_")}><a
-                  href={item.link}>{item.label}</a></li>)
+      return (<ul>{items.items.map(item => {
+        return (
+          <li key={item.label.split(" ").join("_")}>
+            <a rel="external" href={item.link}>{item.label}</a>
+          </li>
+          )
       })}</ul>)
   }
 }
 const getAccordions = list => {
   return list.map(items =>  (
-      <Accordion key={items.title.split(" ").join("_")} summary={items.title}>
+      <Accordion key={items.title.split(" ").join("_")}
+        className={items.type + "-list"}
+        summary={items.title}>
         {items.items && items.items.length ?
           getAccordionContent(items) :
           (<div>No Results found</div>)}
