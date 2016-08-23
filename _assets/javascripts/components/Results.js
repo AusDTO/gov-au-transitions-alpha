4186c7f2c96bd1b2b95460db947f6564ee77d91b
Alex Riolo <alexriolo@DTOAdmins-MacBook-Pro-8.local>
2016-08-23 11:41:19 +1000

    style changes for the results page

--- _assets/javascripts/components/Results.js
+++ _assets/javascripts/components/Results.js
@@ -19,15 +19,14 @@ const Results = ({show}) => {
             <ResultsContentsLinker />
             <ResultSetLinker />
             <button>Print all results  <i className="fa fa-chevron-right" aria-hidden="true"></i></button>
-            <h2>Also explore</h2>
-            <ul>
+            <h2 className="secondary-title">Also explore</h2>
+            <ul className="secondary-links">
               <li><a href="">Managing your medicines</a></li>
               <li><a href="">Financial planning, seminars and advice</a></li>
               <li><a href="">Organising consents such as powers of attorney</a></li>
             </ul>
-            <hr />
-            <form className="list-horizontal" action="" method="post">
-              <h2>Email a friend</h2>
+            <form className="form-wrapper" action="" method="post">
+              <h3>Email a friend</h3>
               <p>Complete the below if you would like to send a link to this guided results page. We will not store any personal information.</p>
 
               <p>
