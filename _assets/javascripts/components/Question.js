87dc718fdfdeaf8d20b7f184abf748c5231fe324
Alex Riolo <alexriolo@DTOAdmins-MacBook-Pro-8.local>
2016-08-23 10:37:33 +1000

    fix for aside nav showing on results page

--- _assets/javascripts/components/Question.js
+++ _assets/javascripts/components/Question.js
@@ -3,14 +3,21 @@ import QuestionHeaderHandler from '../containers/QuestionHeaderHandler'
 import QuestionBodyLinker from '../containers/QuestionBodyLinker'
 import AsideNav from './AsideNav'
 
-const Question = () => (
-  <main role="main">
-    <AsideNav results={false}/>
-    <section className="transition-main content-main">
-      <QuestionHeaderHandler />
-      <QuestionBodyLinker />
-    </section>
-  </main>
-)
+const Question = ({current, total}) => {
+  if (current >= total) {
+    return (<div />)
+  } else {
+    return (
+      <main role="main">
+        <AsideNav results={false}/>
+        <section className="transition-main content-main">
+          <QuestionHeaderHandler />
+          <QuestionBodyLinker />
+        </section>
+      </main>
+    )
+  }
+
+}
 
 export default Question
