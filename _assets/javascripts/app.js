61a628e81ce62c3c687feb16e00d47bb334273b0
Alex Riolo <alexriolo@DTOAdmins-MacBook-Pro-8.local>
2016-08-29 13:20:16 +1000

    added routing to all pages

--- _assets/javascripts/app.js
+++ _assets/javascripts/app.js
@@ -1,25 +1,70 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, compose, combineReducers } from 'redux';
 import transitionApp from './reducers'
 import QuestionLinker from './containers/QuestionLinker'
 import ResultsCalculator from './containers/ResultsCalculator'
-// import { QuestionFlow } from './questions'
-// import { moveBack } from './actions'
+import LandingPage from './components/LandingPage'
+//------------------------------------------------------------------------------
+// have a look at the imorts below this line.not all may be reqired.
+//------------------------------------------------------------------------------
+import {
+  ReduxRouter,
+  routerStateReducer,
+  reduxReactRouter,
+  push,
+} from 'redux-router';
+import { Route } from 'react-router';
+import { createHistory } from 'history';
 
-let store = createStore(transitionApp)
+//let store = createStore(transitionApp)
+// debugger
+const reducer = combineReducers({
+  router: routerStateReducer,
+  transitionApp
+});
 
-render(
-  <Provider store={store}>
-    <QuestionLinker />
-  </Provider>,
-  document.getElementById('questions')
-)
+const store = compose(
+  reduxReactRouter({ createHistory })
+)(createStore)(reducer);
 
-render(
-  <Provider store={store}>
-    <ResultsCalculator />
-  </Provider>,
-  document.getElementById('results-holder')
-)
+const TransitionRouter = () => {
+  return (
+    <Provider store={store}>
+      <ReduxRouter>
+        <Route path="/" component={LandingPage}>
+
+          {/* <Route path="parent" component={Parent}>
+          //   <Route path="child" component={Child} />
+          //   <Route path="child/:id" component={Child} />
+          // </Route>*/}
+        </Route>
+        <Route path="/question" component={QuestionLinker} />
+      </ReduxRouter>
+    </Provider>
+  )
+}
+
+render(<TransitionRouter />, document.getElementById('landing-page'))
+
+// render(
+//   <Provider store={store}>
+//     <QuestionLinker />
+//   </Provider>,
+//   document.getElementById('questions')
+// )
+//
+// render(
+//   <Provider store={store}>
+//     <ResultsCalculator />
+//   </Provider>,
+//   document.getElementById('results-holder')
+// )
+
+// render(
+//   <Provider store={store}>
+//     <LandingPage />
+//   </Provider>,
+//   document.getElementById('landing-page')
+// )
