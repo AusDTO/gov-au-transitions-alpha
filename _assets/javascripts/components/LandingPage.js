0b0ca09d81ccc779fdbb326fb513c21c1374e6c4
Nathan Flew <nath@nathf.com>
2016-09-05 15:28:09 +1000

    Landing page updates

--- _assets/javascripts/components/LandingPage.js
+++ _assets/javascripts/components/LandingPage.js
@@ -9,36 +9,57 @@ const LandingPage = () => {
           <div className="landing-wrapper">
             <h2>Help for older people</h2>
             <h1>Answer a few questions about your situation to see what government help you can get, and how to go about it.</h1>
-            <Link to="/question">Next<i className="fa fa-chevron-right" aria-hidden="true"></i></Link>
+            <Link to="/question" role="button">Next <i className="fa fa-chevron-right" aria-hidden="true"></i></Link>
           </div>
         </article>
       </main>
-      <aside className="asside-content">
-        <section className="wrapper">
+      <main>
+        <article>
           <h2>Related topics</h2>
-          <div className="topics-wrapper">
-            <article className="related-topics">
-              <a href="/">Choosing a service provider</a>
-              <p>How to choose a provider for home help, things to consider and a checklist of questions to ask a them.</p>
-            </article>
-
-            <article className="related-topics">
-              <a href="/">Getting assessed for home support</a>
-              <p>Assessment for home services is done by a service provider who will come to your home. Call My Aged Care to get started.</p>
-            </article>
-
-            <article className="related-topics">
-              <a href="/">Organising consents</a>
-              <p>There are a few ways to appoint someone legally to make decisions on your behalf, in the short and long term.</p>
-            </article>
-
-            <article className="related-topics">
-              <a href="/">Personal alarms</a>
-              <p>They can be used in an emergency to alert a contact such as a neighbour, relative or monitoring service.</p>
-            </article>
-          </div>
-        </section>
-      </aside>
+            <ul className="list-vertical--fourths real-list">
+              <li>
+                <article>
+                  <h3>
+                    <a href="/">Choosing a service provider</a>
+                  </h3>
+                  <p>
+                    How to choose a provider for home help, things to consider and a checklist of questions to ask a them.
+                  </p>
+                </article>
+              </li>
+              <li>
+                <article>
+                  <h3>
+                    <a href="/">Getting assessed for home support</a>
+                  </h3>
+                  <p>
+                    Assessment for home services is done by a service provider who will come to your home. Call My Aged Care to get started.
+                  </p>
+                </article>
+              </li>
+              <li>
+                <article>
+                  <h3>
+                    <a href="/">Organising consents</a>
+                  </h3>
+                  <p>
+                    There are a few ways to appoint someone legally to make decisions on your behalf, in the short and long term.
+                  </p>
+                </article>
+              </li>
+              <li>
+                <article>
+                  <h3>
+                    <a href="/">Personal alarms</a>
+                  </h3>
+                  <p>
+                    They can be used in an emergency to alert a contact such as a neighbour, relative or monitoring service.
+                  </p>
+                </article>
+              </li>
+            </ul>
+        </article>
+      </main>
     </div>
   )
 }
