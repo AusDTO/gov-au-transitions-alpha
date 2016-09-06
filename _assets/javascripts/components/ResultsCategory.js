4c559667e649142456c51704c76d75a848372eab
Nathan Flew <nath@nathf.com>
2016-09-06 10:49:58 +1000

    Add missing changes to indepth category page

--- _assets/javascripts/components/ResultsCategory.js
+++ _assets/javascripts/components/ResultsCategory.js
@@ -1,28 +1,38 @@
 import React from 'react'
 import ResultSet from './ResultSet'
 import AsideNavLink from '../containers/AsideNavLink'
+import ResultsMenu from './ResultsMenu'
 
-const ResultsCategory = ({ show, result, resultSteps, onChange }) => {
-  if (show) {
-    return (
-      <div>
-        <div className="hero-sml">
-          <div className="wrapper">
-            <p>Explore your results</p>
-          </div>
+const ResultsCategory = ({ show, result, resultSteps, showResults, resultSets, onChange }) => {
+  if (!show) {
+    return (<div />)
+  }
+
+  return (
+    <div>
+      <div className="hero-sml">
+        <div className="wrapper">
+          <p>Explore your results</p>
         </div>
-        <main role="main">
-          <AsideNavLink />
+      </div>
+      <main role="main">
+        <article>
+          <aside className="sidebar">
+            {showResults ? <ResultsMenu resultSets={resultSets}/> : ''}
+            <AsideNavLink className="hide-for-mobile" />
+          </aside>
           <article className="transition-main content-main">
             <ResultSet result={result} resultSteps={resultSteps}
                         onChange={onChange} />
           </article>
-        </main>
-      </div>
-    )
-  } else {
-    return (<div />)
-  }
+        </article>
+        <article>
+            <AsideNavLink className="show-for-mobile" />
+        </article>
+      </main>
+    </div>
+  )
+
 }
 
 export default ResultsCategory
