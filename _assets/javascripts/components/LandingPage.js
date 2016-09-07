d36ce7bb9837496f004b1531e4188f159ae344a2
Nathan Flew <nath@nathf.com>
2016-09-07 11:49:48 +1000

    Move related topics into a structure for easier layout manipulation

--- _assets/javascripts/components/LandingPage.js
+++ _assets/javascripts/components/LandingPage.js
@@ -1,6 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router';
 
+const relatedTopics = [
+  {
+    title: 'Choosing a service provider',
+    content: 'How to choose a provider for home help, things to consider and a checklist of questions to ask a them.'
+  },
+  {
+    title: 'Getting assessed for home support',
+    content: 'Assessment for home services is done by a service provider who will come to your home. Call My Aged Care to get started.'
+  },
+  {
+    title: 'Organising consents',
+    content: 'There are a few ways to appoint someone legally to make decisions on your behalf, in the short and long term.'
+  },
+  {
+    title: 'Personal alarms',
+    content: 'They can be used in an emergency to alert a contact such as a neighbour, relative or monitoring service.'
+  }
+]
+
 const LandingPage = () => {
   return (
     <div>
@@ -20,46 +39,16 @@ const LandingPage = () => {
         <article>
           <h2>Related topics</h2>
             <ul className="list-vertical--fourths real-list">
-              <li>
-                <article>
-                  <h3>
-                    <a href="/">Choosing a service provider</a>
-                  </h3>
-                  <p>
-                    How to choose a provider for home help, things to consider and a checklist of questions to ask a them.
-                  </p>
-                </article>
-              </li>
-              <li>
-                <article>
-                  <h3>
-                    <a href="/">Getting assessed for home support</a>
-                  </h3>
-                  <p>
-                    Assessment for home services is done by a service provider who will come to your home. Call My Aged Care to get started.
-                  </p>
-                </article>
-              </li>
-              <li>
-                <article>
-                  <h3>
-                    <a href="/">Organising consents</a>
-                  </h3>
-                  <p>
-                    There are a few ways to appoint someone legally to make decisions on your behalf, in the short and long term.
-                  </p>
-                </article>
-              </li>
-              <li>
-                <article>
-                  <h3>
-                    <a href="/">Personal alarms</a>
-                  </h3>
-                  <p>
-                    They can be used in an emergency to alert a contact such as a neighbour, relative or monitoring service.
-                  </p>
-                </article>
-              </li>
+              {relatedTopics.map(({ title, content }) => (
+                <li>
+                  <article>
+                    <h3>
+                      <a href="/">{title}</a>
+                    </h3>
+                    <p>{content}</p>
+                  </article>
+                </li>
+              ))}
             </ul>
         </article>
       </main>
