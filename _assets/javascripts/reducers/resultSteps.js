6cb747dc4ee82d7d585215c49c4566979bf7a099
Nathan Flew <nath@nathf.com>
2016-09-16 15:44:35 +1000

    Various fixes

--- _assets/javascripts/reducers/resultSteps.js
+++ _assets/javascripts/reducers/resultSteps.js
@@ -8,6 +8,9 @@ export default function resultSteps (state = [], action) {
   switch (type) {
     case RESULT_CHECK:
       return getResultSteps(step, state)
+    case '@@reduxReactRouter/routerDidChange':
+      window.scrollTo(0, 0)
+      return state
     default:
       return state
   }
